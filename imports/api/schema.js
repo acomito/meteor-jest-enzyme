4b9137550f9c60b139ee25c02698290fde484bed
xavcz <xavier.cazalot@gmail.com>
2017-03-13 13:31:57 +0100

    use an accounts package running without blaze Demonstrate the use of withApollo + client.resetStore(),

--- imports/api/schema.js
+++ imports/api/schema.js
@@ -1,6 +1,7 @@
 import { Random } from 'meteor/random';
 
-export const typeDefs = [`
+export const typeDefs = [
+  `
 type Email {
   address: String
   verified: Boolean
@@ -8,31 +9,25 @@ type Email {
 
 type User {
   emails: [Email]
-  username: String
   randomString: String
   _id: String
 }
 
 type Query {
-  user(id: String!): User
+  user: User
 }
-
-schema {
-  query: Query
-}
-`];
+`,
+];
 
 export const resolvers = {
   Query: {
     user(root, args, context) {
       // Only return the current user, for security
-      if (context.userId === args.id) {
-        return context.user;
-      }
+      return context.user;
     },
   },
   User: {
-    emails: ({emails}) => emails,
+    emails: ({ emails }) => emails,
     randomString: () => Random.id(),
-  }
-}
+  },
+};
--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,40 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { graphql } from 'react-apollo';
-import { Meteor } from 'meteor/meteor';
-import { createContainer } from 'meteor/react-meteor-data';
-import { LoginButtons } from 'meteor/okgrow:accounts-ui-react';
 import gql from 'graphql-tag';
 
-Accounts.ui.config({
-  passwordSignupFields: 'USERNAME_ONLY'
-});
+import LoginForm from './LoginForm';
 
-const App = ({ userId, currentUser, refetch }) => (
+const App = ({ currentUser, refetch }) => (
   <div>
-    <LoginButtons visible />
-    {userId ? (
-      <div>
-        <pre>{JSON.stringify(currentUser, null, 2)}</pre>
-        <button onClick={() => refetch()}>Refetch!</button>
-      </div>
-    ) : 'Please log in!'}
+    <LoginForm />
+
+    {currentUser
+      ? <div>
+          <pre>{JSON.stringify(currentUser, null, 2)}</pre>
+          <button onClick={() => refetch()}>Refetch!</button>
+        </div>
+      : 'Please log in!'}
   </div>
 );
 
 App.propTypes = {
-  userId: React.PropTypes.string.isRequired,
   currentUser: React.PropTypes.object,
   refetch: React.PropTypes.func,
 };
 
 const GET_USER_DATA = gql`
-  query ($id: String!) {
-    user(id: $id) {
+  query getCurrentUser {
+    user {
       emails {
         address
         verified
       }
-      username
       randomString
       _id
     }
@@ -43,25 +37,20 @@ const GET_USER_DATA = gql`
 
 const withData = graphql(GET_USER_DATA, {
   props: ({ data: { error, loading, user, refetch } }) => {
+    // xxx: display loading
     if (loading) return { userLoading: true };
+    // xxx: display errors
     if (error) return { hasErrors: true };
+
     return {
       currentUser: user,
       refetch,
     };
   },
-  options: (ownProps) => (
-    { variables: { id: ownProps.userId } }
-  ),
+  // xxx: show example
+  // options: (ownProps) => (
+  //   { variables: { id: ownProps.userId } }
+  // ),
 });
 
-const AppWithData = withData(App);
-
-// This container brings in Tracker-enabled Meteor data
-const AppWithUserId = createContainer(() => {
-  return {
-    userId: Meteor.userId() || '',
-  };
-}, AppWithData);
-
-export default AppWithUserId;
+export default withData(App);
